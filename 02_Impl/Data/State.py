f8691f833f42d29ee8199c479868dad607637396
Taiye Jebutu <taiyejebutu@icloud.com>
2025-04-22 12:58:18 +0100

    Data class for State.py has been added

---
+++ 02_Impl/Data/State.py
@@ -0,0 +1,57 @@
+# Standard
+import dataclasses
+from dataclasses import dataclass
+
+
+# External
+
+# Local
+
+@dataclass
+class State:
+    valid: bool = dataclasses.Field(default=False)
+    aileron: float = dataclasses.Field(default=0.0)
+    elevator: float = dataclasses.Field(default=0.0)
+    rudder: float = dataclasses.Field(default=0.0)
+    flaps: float = dataclasses.Field(default=0.0)
+    h_m: float = dataclasses.Field(default=0.0)
+    tas: float = dataclasses.Field(default=0.0)
+    ias: float = dataclasses.Field(default=0.0)
+    m: float = dataclasses.Field(default=0.0)
+    aoa_deg: float = dataclasses.Field(default=0.0)
+    aos_deg: float = dataclasses.Field(default=0.0)
+    ny: float = dataclasses.Field(default=0.0)
+    vy: float = dataclasses.Field(default=0.0)
+    wx: float = dataclasses.Field(default=0.0)
+    mfuel: float = dataclasses.Field(default=0.0)
+    mfuel_0: float = dataclasses.Field(default=0.0)
+    throttle_1: float = dataclasses.Field(default=0.0)
+    throttle_2: float = dataclasses.Field(default=0.0)
+    throttle_3: float = dataclasses.Field(default=0.0)
+    mixture_1: float = dataclasses.Field(default=0.0)
+    mixture_2: float = dataclasses.Field(default=0.0)
+    mixture_3_: float = dataclasses.Field(default=0.0)
+    magneto_1: float = dataclasses.Field(default=0.0)
+    magneto_2: float = dataclasses.Field(default=0.0)
+    magneto_3: float = dataclasses.Field(default=0.0)
+    power_1: float = dataclasses.Field(default=0.0)
+    power_2: float = dataclasses.Field(default=0.0)
+    power_3: float = dataclasses.Field(default=0.0)
+    rpm1: float = dataclasses.Field(default=0.0)
+    rpm_2: float = dataclasses.Field(default=0.0)
+    rpm_3: float = dataclasses.Field(default=float)
+    manifold_pressure_1: float = dataclasses.Field(default=0.0)
+    manifold_pressure_2: float = dataclasses.Field(default=0.0)
+    manifold_pressure_3: float = dataclasses.Field(default=0.0)
+    oil_temp_1: float = dataclasses.Field(default=0.0)
+    oil_temp_2: float = dataclasses.Field(default=0.0)
+    oil_temp_3: float = dataclasses.Field(default=0.0)
+    pitch_1_deg: float = dataclasses.Field(default=0.0)
+    pitch_2_deg: float = dataclasses.Field(default=0.0)
+    pitch_3_deg: float = dataclasses.Field(default=0.0)
+    thrust_1: float = dataclasses.Field(default=0.0)
+    thrust_2: float = dataclasses.Field(default=0.0)
+    thrust_3: float = dataclasses.Field(default=0.0)
+    efficiency_1: float = dataclasses.Field(default=0.0)
+    efficiency_2: float = dataclasses.Field(default=0.0)
+    efficiency_3: float = dataclasses.Field(default=0.0)
