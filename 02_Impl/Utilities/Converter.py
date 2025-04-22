ac61daa725c452e53480a983ab8aceefe5c956b4
Taiye Jebutu <taiyejebutu@icloud.com>
2025-04-22 17:27:59 +0100

    Added converter method

---
+++ 02_Impl/Utilities/Converter.py
@@ -0,0 +1,13 @@
+# Standard
+
+# External
+
+# Local
+
+mapping = {
+
+}
+
+
+def convert(data: dict[str, str], target):
+    """Converts data into its target type"""
---
+++ 02_Impl/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="WTData",
+    version="0.1",
+    description="WTData Package",
+    packages=['WTData']
+)
